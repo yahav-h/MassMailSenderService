c6e0a341fd059b8281e611daf4ca55645a95c7f6
Yahav-N-Hoffman <yahavh@avanan.com>
2021-12-28 21:32:44 +0200

    update main.py

--- main.py
+++ main.py
@@ -32,11 +32,20 @@ async def add_x_response_id_header(req: Request, call_next):
     res.headers["X-RESPONSE-ID"] = common.gen_id()
     return res
 
+@app.on_event('startup')
+async def test_service():
+    assert helpers.exists(helpers.join(helpers.config.template_folder_path, 'index.html'))
+    assert helpers.exists(helpers.join(helpers.config.template_folder_path, 'template.eml'))
+    assert helpers.exists(helpers.join(helpers.config.resources_folder_path, 'properties.json'))
+    props = helpers.config.get_resource('properties.json')
+    for v in props.values():
+        assert v != ""
+
 @app.get('/')
 async def index(req: Request): return templates.TemplateResponse('index.html', {"request": req})
 
-@app.post('/api/sendmail')
-async def sendmail(req: Request):
+@app.post('/api/tasks')
+async def start_task(req: Request):
     form_data = await req.form()
     if all([v != '' for k, v in form_data._dict.items()]):
         task_id = common.gen_id()
@@ -62,6 +71,20 @@ async def sendmail(req: Request):
             media_type="application/json",
             headers={"content-type": "application/json"}
         )
+
+@app.get('/api/tasks')
+async def get_task(req: Request):
+    """ TODO : Future implementation for getting a task status """
+    return JSONResponse(
+        content=jsonable_encoder({
+            "status": "Not Implemented Yet!",
+            "timestamp": common.stamp(),
+            "requester": req.client.host
+        }),
+        status_code=200,
+        media_type="application/json",
+        headers={"content-type": "application/json"}
+    )
 
 if __name__ == '__main__':
     from uvicorn import run
